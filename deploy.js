44ec84eb744e45139266c3e0a9073c3f30c493d1
bingxl <2282152858@qq.com>
2017-08-05 13:51:44 +0800

    add deploy tool and node工具 articl

--- deploy.js
+++ deploy.js
@@ -1,10 +1,25 @@
+/**
+ * @type {array} 命令行输入命令数组
+ */
 var argv = process.argv;
 let fs = require('fs');
 let EventEmitter = require('events').EventEmitter;
 let event = new EventEmitter();
+
+/**
+ * @type {Array} 获取命令行输入参数
+ */
 let com = argv.splice(2);
 var doex = require('child_process').exec; 
+
+/**
+ * @type {Array} 事件队列
+ */
 let eventList = [];
+
+/**
+ * @type {Boolean} 事件执行标志
+ */
 let hasEventDo = false;
 
 if(com.toString()){
@@ -20,7 +35,12 @@ if(com.toString()){
 }
 
 event.emit('exec');
-function exec (com, callback){
+
+/**
+ * 执行命令行
+ * @param {string} com 执行的命令参数
+ */
+function exec (com){
     hasEventDo = true;
     doex(com, function(err,stdout,stderr){
         if(err) {
@@ -33,13 +53,25 @@ function exec (com, callback){
     });
 }
 
+/**
+ * 将数据打印到控制台
+ * @param {*} str 打印数据
+ */
 function log(str) {
     console.log(str);
 }
 // log(process);
-event.on('exec', function() {
+event.on('exec', function(delayNnm = 0) {
     if(hasEventDo){
-        return -1;
+        // 如果已有事物执行则延迟五秒
+        if(delayNum > 1){
+            // 已循环一次后则认为有问题，结束调用
+            log("已有事件占用太长时间");
+            return -1;
+        }
+        setTimeout(function(){
+            event.emit('exec', delayNum++);
+        },5000);
     }
     if(eventList.length){
         let eventName = eventList.shift();
