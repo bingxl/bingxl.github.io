2600a726afd4c5bc5e727d340f2d5be0226e705d
bingxl <2282152858@qq.com>
2018-11-19 18:35:19 +0800

    add https站点提交

--- urlsGenerate.js
+++ urlsGenerate.js
@@ -1,6 +1,6 @@
 let fs = require('fs');
 let os = require('os');
-let site = 'https://bingxl.cn/';
+let site = 'https://www.bingxl.cn/';
 let data = site + os.EOL;
 fs.readdir('./source/_posts/', (err, files) => {
     files.forEach(item => {
